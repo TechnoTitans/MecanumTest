40d18350aaedc203a0df07bb6593773d033fe29a
js2585 <jatongsu2003@gmail.com>
2018-11-13 16:40:26 -0500

    Not working

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,7 +7,9 @@
 
 package frc.robot;
 
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.drive.MecanumDrive;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
@@ -17,6 +19,19 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  * documentation. If you change the name of this class or the package after
  * creating this project, you must also update the build.gradle file in the
  * project.
+ */
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.TimedRobot;
+
+/*
+ * Simplest program to drive a robot with mecanum drive using a single Logitech
+ * Extreme 3D Pro joystick and 4 drive motors connected as follows:
+ *     - PWM 0 - Connected to front left drive motor
+ *     - PWM 1 - Connected to rear left drive motor
+ *     - PWM 2 - Connected to front right drive motor
+ *     - PWM 3 - Connected to rear right drive motor
  */
 public class Robot extends TimedRobot {
   private static final String kDefaultAuto = "Default";
@@ -28,6 +43,23 @@ public class Robot extends TimedRobot {
    * This function is run when the robot is first started up and should be
    * used for any initialization code.
    */
+  //Create a robot drive object using PWMs 0, 1, 2 and 3
+  TalonSRX m_frontLeft = new TalonSRX(1);
+  TalonSRX m_rearLeft = new TalonSRX(2);
+  TalonSRX m_frontRight = new TalonSRX(3);
+  TalonSRX m_rearRight = new TalonSRX(4);
+  //Define joystick being used at USB port 1 on the Driver Station
+  Joystick m_driveStick = new Joystick(1);
+
+  /**
+   * This function is called periodically during operator control.
+   */
+
+    @Override
+  public void teleopPeriodic() {
+        m_robotDrive = new MecanumDrive(m_frontLeft, m_rearLeft, m_frontRight, m_rearRight);
+        m_robotDrive.mecanumDrive_Cartesian(m_driveStick.getX(), m_driveStick.getY(), m_driveStick.getZ());
+  }
   @Override
   public void robotInit() {
     m_chooser.addDefault("Default Auto", kDefaultAuto);
@@ -81,12 +113,7 @@ public class Robot extends TimedRobot {
     }
   }
 
-  /**
-   * This function is called periodically during operator control.
-   */
-  @Override
-  public void teleopPeriodic() {
-  }
+
 
   /**
    * This function is called periodically during test mode.
